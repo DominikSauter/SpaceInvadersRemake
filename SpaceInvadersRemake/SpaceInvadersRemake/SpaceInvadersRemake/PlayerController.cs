a56ae89e71a3b67cc7bab39735949091ab2391a7
unrealegon <unrealegon@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-05-23 19:48:49 +0000

    - Abstract Controller Class hinzugefügt     - Methoden     - Feldern für alle Bewegunsrichtungen - Unterklassen der Controller Class hinzugefügt - Der Klasse IGameItem "using Microsoft.Xna.Framework;" hinzugefügt damit sie typ Vector2 kennt

---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvadersRemake
+{
+    public abstract class PlayerController : Controller
+    {
+        public UserInput UserInput
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
