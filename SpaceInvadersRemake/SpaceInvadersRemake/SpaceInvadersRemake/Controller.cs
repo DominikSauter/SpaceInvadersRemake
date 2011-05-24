145523762556bc58073a9fd73546fe99d4c5c256
unrealegon <unrealegon@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-05-24 17:41:30 +0000

    -changed field modifier in controller from private to protected

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller.cs
@@ -10,14 +10,14 @@ namespace SpaceInvadersRemake
     public abstract class Controller
     {
         //Directions Definitions
-        private readonly Vector2 Right = new Vector2(1,0);
-        private readonly Vector2 Up = new Vector2(0,1);
-        private readonly Vector2 UpRight = new Vector2(1,1);
-        private readonly Vector2 Left = new Vector2(-1,0);
-        private readonly Vector2 Down = new Vector2(0,-1);
-        private readonly Vector2 DownLeft = new Vector2(-1,-1);
-        private readonly Vector2 DownRight = new Vector2(1,-1);
-        private readonly Vector2 UpLeft = new Vector2(-1,1);
+        protected readonly Vector2 Right = new Vector2(1, 0);
+        protected readonly Vector2 Up = new Vector2(0, 1);
+        protected readonly Vector2 UpRight = new Vector2(1, 1);
+        protected readonly Vector2 Left = new Vector2(-1, 0);
+        protected readonly Vector2 Down = new Vector2(0, -1);
+        protected readonly Vector2 DownLeft = new Vector2(-1, -1);
+        protected readonly Vector2 DownRight = new Vector2(1, -1);
+        protected readonly Vector2 UpLeft = new Vector2(-1, 1);
     
         public Controller(IGameItem controls)
         {
