2cd39d5d65752567fe29b77c48db49f4f9d8d92e
unrealegon <unrealegon@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-05-25 11:03:15 +0000

    -Outsourced Directions from Controller to own Static class

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller.cs
@@ -9,16 +9,7 @@ namespace SpaceInvadersRemake
     
     public abstract class Controller
     {
-        //Directions Definitions
-        protected readonly Vector2 Right = new Vector2(1, 0);
-        protected readonly Vector2 Up = new Vector2(0, 1);
-        protected readonly Vector2 UpRight = new Vector2(1, 1);
-        protected readonly Vector2 Left = new Vector2(-1, 0);
-        protected readonly Vector2 Down = new Vector2(0, -1);
-        protected readonly Vector2 DownLeft = new Vector2(-1, -1);
-        protected readonly Vector2 DownRight = new Vector2(1, -1);
-        protected readonly Vector2 UpLeft = new Vector2(-1, 1);
-    
+  
         public Controller(IGameItem controls)
         {
             throw new System.NotImplementedException();
---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/DirectionsConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SpaceInvadersRemake
+{
+    public static class DirectionsConstants
+    {
+        //Directions Definitions
+        public static Vector2 right = new Vector2(1, 0);
+        public static Vector2 up = new Vector2(0, 1);
+        public static Vector2 upRight = new Vector2(1, 1);
+        public static Vector2 left = new Vector2(-1, 0);
+        public static Vector2 down = new Vector2(0, -1);
+        public static Vector2 downLeft = new Vector2(-1, -1);
+        public static Vector2 downRight = new Vector2(1, -1);
+        public static Vector2 upLeft = new Vector2(-1, 1);
+  
+
+    }
+}
