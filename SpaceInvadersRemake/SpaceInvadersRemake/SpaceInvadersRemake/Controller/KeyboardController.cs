93917e8800bd6a3002fdf21c51d2bf922ef30329
unrealegon <unrealegon@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-05-26 22:58:09 +0000

    -Abstrakte Controller Klasse kommentiert und VS Bugs behoben (nicht abstrakte zu abstrakte methoden geändert) - Unterklassen von Controller haben abstrakte methoden impementiert (leerer rumpf) -keyboard settings datei erstellt - Klassenleichen aufgeräumt (siehe deleted) -Controller Klassen in Namespace/Ordner "Controller" verschoben

---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller/KeyboardController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvadersRemake
+{
+    public class KeyboardController : PlayerController
+    {
+        public Settings.Keyboard KBconfig
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        
+      
+      
+        public override IGameItem Controllee
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Microsoft.Xna.Framework.Vector2 CheckMovement()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool CheckShooting()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
