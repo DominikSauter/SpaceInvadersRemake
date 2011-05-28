3e74183bc5ffcdbec00a38b5ae78f36c61c6c730
unrealegon <unrealegon@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-05-28 13:10:15 +0000

    -Controller kommentare verfasst (Userinput Pfad) -GameItem zur abstrakten Klasse geändert (Methoden abstrakt gemacht)

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller/KeyboardController.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller/KeyboardController.cs
@@ -5,8 +5,22 @@ using System.Text;
 
 namespace SpaceInvadersRemake
 {
+
+    /// <summary>
+    /// Diese Klasse stellt die konkrete Benutzereingabe mithilfe der Tastatur.
+    /// </summary>
+    /// <remarks>In dieser Klasse wird das Verhalten des Controllers vom einem menschlichen Benutzers
+    /// mithilfe der Tastatur bestimmt</remarks>
     public class KeyboardController : PlayerController
     {
+
+       
+
+
+        /// <summary>
+        /// Eigenschaft der Tastatur Konfiguration
+        /// </summary>
+        /// 
         public Settings.Keyboard KBconfig
         {
             get
@@ -18,9 +32,13 @@ namespace SpaceInvadersRemake
             }
         }
 
+       
         
-      
-      
+        
+
+        /// <summary>
+        /// Eigenschaft Controllee (kontrollierte Objekt)
+        /// </summary>
         public override IGameItem Controllee
         {
             get
@@ -33,16 +51,36 @@ namespace SpaceInvadersRemake
             }
         }
 
+        /// <summary>
+        ///   <c>Update</c>ist die Methode, die  pro Frame aufgerufen wird, damit entschieden wird wie sich Controllee verhalten soll
+        /// </summary>
         public override void Update()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Entscheided in welche Richtung sich das Controllee bewegen soll
+        /// </summary>
+        /// <remarks>Dies geschieht indem der Benutzer die Tasten für die Bewegungsrichtungen drückt.
+        /// Welche Tasten dies sind, ist in der Eigenschaft <c>KBconfig</c> hinterlegt</remarks>
+        /// <returns>
+        /// 2D Richtungsvektor
+        /// </returns>
         protected override Microsoft.Xna.Framework.Vector2 CheckMovement()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Entscheided ob Controllee schießen soll
+        /// </summary>
+        /// <remarks>Dies geschieht indem der Benutzer die Taste für schießen drückt.
+        /// Welche Taste dies ist, ist in der Eigenschaft <c>KBconfig</c> hinterlegt
+        /// </remarks>
+        /// <returns>
+        ///   <c>true</c> = schießen andererseits <c>false</c>
+        /// </returns>
         protected override bool CheckShooting()
         {
             throw new NotImplementedException();
--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller/PlayerController.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller/PlayerController.cs
@@ -5,7 +5,55 @@ using System.Text;
 
 namespace SpaceInvadersRemake
 {
+    /// <summary>
+    /// Diese Klasse abstrahiert  von den verschiedenen Benutzereingaben
+    /// </summary>
+    /// <remarks>Verhalten des Controllers wird durch Benutzereingabe bestimmt</remarks>
     public abstract class PlayerController : Controller
     {
+        /// <summary>
+        /// Eigenschaft Controllee (kontrollierte Objekt)
+        /// </summary>
+        public override IGameItem Controllee
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        ///   <c>Update</c>ist die Methode, die  pro Frame aufgerufen wird, damit entschieden wird wie sich Controllee verhalten soll
+        /// </summary>
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Entscheided in welche Richtung sich das Controllee bewegen soll
+        /// </summary>
+        /// <returns>
+        /// 2D Richtungsvektor
+        /// </returns>
+        protected override Microsoft.Xna.Framework.Vector2 CheckMovement()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Entscheided ob Controllee schießen soll
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> = schießen andererseits <c>false</c>
+        /// </returns>
+        protected override bool CheckShooting()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
