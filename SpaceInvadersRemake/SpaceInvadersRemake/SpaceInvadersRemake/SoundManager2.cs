36e39a3e116947644f52bf6b983c6f3606cf7c74
moepdodo211 <moepdodo211@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-05-28 17:25:42 +0000

    ViewConstants erweitert. Soundsystem bearbeitet

---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/SoundManager2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvadersRemake
+{
+    public interface SoundManager2
+    {
+        float Volume
+        {
+            get;
+            set;
+        }
+
+        void play(float volume);
+    }
+}
