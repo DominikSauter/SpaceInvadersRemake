ef8c432f0c1da7d06afe2025460471fad765a9f8
moepdodo211 <moepdodo211@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-05-28 16:01:52 +0000

    ParticleEngine hinzugefügt und Particle angepasst. ClassDiagram fehlerhaften XML Teil entfernt

---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/View/ParticleEngine.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceInvadersRemake
+{
+    public class ParticleEngine
+    {
+        private Random random;
+        private List<Particle> particles;
+        private List<Texture2D> textures;
+
+        public ParticleEngine(System.Collections.Generic.List<Texture2D> textures, Vector2 location)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Vector2 EmitterLocation
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        private Particle GenerateNewParticle()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
