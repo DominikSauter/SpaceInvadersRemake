ef66680bca4aa5749d398ea6243010e864daadea
moepdodo211 <moepdodo211@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-05-29 18:30:14 +0000

    MediaPlayer hinzugefügt

---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Mediaplayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvadersRemake
+{
+    public interface Mediaplayer
+    {
+        float Volume
+        {
+            get;
+            set;
+        }
+    
+        void play();
+    }
+}
