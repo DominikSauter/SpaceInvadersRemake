80925aa33f58c640623877553f5224c28c891917
moepdodo211 <moepdodo211@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-05-29 18:00:55 +0000

    View-Konstanten hinzugefügt

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/View/ViewConstants.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/View/ViewConstants.cs
@@ -131,6 +131,259 @@ namespace SpaceInvadersRemake
             }
         }
 
+        public static Model PlayerModel
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Model BossModel
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Model Alien1Model
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Model Alien2Model
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Model Alien3Model
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Model Alien4Model
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Model Alien5Model
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Model MothershipModel
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static ModelHitsphere Alien1Hitsphere
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static ModelHitsphere Alien2Hitsphere
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static ModelHitsphere Alien3Hitsphere
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static ModelHitsphere Alien4Hitsphere
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static ModelHitsphere Alien5Hitsphere
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static ModelHitsphere MothershipHitsphere
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static ModelHitsphere BossHitsphere
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Texture2D PlayerProjectileNormal
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Texture2D AlienProjectile
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Texture2D PowerUp
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Texture2D ShieldTexture
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Texture2D PlayerProjectilePiercing
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Texture2D MothershipProjectile
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Microsoft.Xna.Framework.Graphics.Texture2D BossProjectile
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static int ShieldHitsphere
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
         public void loadContent()
         {
             throw new System.NotImplementedException();
