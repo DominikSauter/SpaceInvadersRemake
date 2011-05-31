dbe799b40421b16459edab1056f1ae7ba9a65304
doominikthunder <doominikthunder@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-05-31 22:48:07 +0000

    [Model] GameCourseManager hinzugefügt zur Verwaltung des Spielablaufs, sowie einige kleinere räumliche Umordnungen für die bessere Lesbarkeit

---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/GameCourseManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvadersRemake
+{
+    public class GameCourseManager
+    {
+        private int waveCounter;
+    
+        public void InitializeGame()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NewWave()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GameOver()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
