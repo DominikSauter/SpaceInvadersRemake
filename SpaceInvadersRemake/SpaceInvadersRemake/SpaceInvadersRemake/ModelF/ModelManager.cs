439880bc5580024357e7b6c766fcf2b65916cb46
unrealegon <unrealegon@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-01 16:39:25 +0000

    moving files

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/ModelF/ModelManager.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/ModelF/ModelManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SpaceInvadersRemake.Model
+namespace SpaceInvadersRemake.ModelF
 {
     class ModelManager
     {
