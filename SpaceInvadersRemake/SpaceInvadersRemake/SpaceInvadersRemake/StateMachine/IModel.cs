bbcf04b9550c01d64a91dd7864d894f35463da79
unrealegon <unrealegon@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-01 14:56:22 +0000

    [State] Interface hinzugefügt, die von den MVCManager Klassen implementiert

---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/StateMachine/IModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvadersRemake.StateMachine
+{
+    interface IModel
+    {
+        void Update(Microsoft.Xna.Framework.Game game, Microsoft.Xna.Framework.GameTime gameTime, State state);
+    }
+}
