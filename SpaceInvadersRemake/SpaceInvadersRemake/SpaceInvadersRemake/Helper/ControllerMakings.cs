806f06c5f763a2e6d11d30e97126296399068d02
unrealegon <unrealegon@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-04 12:53:02 +0000

    [Controller] Klassen auf neue NameSpaces angepasst

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Helper/ControllerMakings.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Helper/ControllerMakings.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SpaceInvadersRemake.Controller;
 
 namespace SpaceInvadersRemake.Helper
 {
@@ -11,5 +12,8 @@ namespace SpaceInvadersRemake.Helper
     /// <remarks>Indem man diese Struct dem Event Model Created mitgibt, steuert wann z.B. welche Controller Intelligens man haben möchte oder wie hoch die Schussfrequenz des Controllers sein soll.</remarks>
     public struct ControllerMakings
     {
+        AIEnum desiredController;
+        int shootingFrequenz;
+
     }
 }
