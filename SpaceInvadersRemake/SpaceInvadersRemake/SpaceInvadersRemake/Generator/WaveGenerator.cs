62dc1c212a3497e4e2fa327590acc1ea39e49094
doominikthunder <doominikthunder@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-04 19:31:34 +0000

    ZURÜCKSETZUNG: Klassendiagramm auf den Stand von Revision 128 zurückgesetzt.

---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Generator/WaveGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SpaceInvadersRemake.Generator
+{
+    /// <summary>
+    /// Die Klasse dient dazu sich Gegner (<c>GameItem</c>) generieren zu lassen
+    /// </summary>
+    /// <remarks>Abstrakte Fabrik</remarks>
+    public static class FormationGenerator
+    {
+        public static Array BlockFormation
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Array SkullFormation
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+       /// <summary>
+        /// Generiert eine FormationEnum von Gegnern 
+       /// </summary>
+       /// <returns></returns>
+        protected static List<IGameItem> CreateFormation(int hitpoints, Vector2 velocity, Array formation)
+        {
+            throw new System.NotImplementedException();
+}
+ 
+    }
+
+    public static class WaveGenerator
+    {
+        public static event EventHandler WaveGenerated;
+    
+        public static List<IGameItem> CreateWave(ControllerEnum AI, Array formation, DifficultyLevel difficultyLevel)
+        {
+            throw new System.NotImplementedException();
+            //SwitchCase über "Bestellung" 
+            //Private Methoden für konkrete Creatings um swichcase übersichtlich zu halten
+        }
+    }
+}
--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/ModelF/Projectile.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/ModelF/Projectile.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace SpaceInvadersRemake
 {
