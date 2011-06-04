73e1899e6c4d6c0631314e9fe9d4293eaef3bc30
doominikthunder <doominikthunder@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-04 02:38:23 +0000

    [Model] Formationsgenerierung umstrukturiert, Event "WaveGenerated" für WaveGenerator hinzugefügt, sowie diverse kleinere Änderungen

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Generator/WaveGenerator.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Generator/WaveGenerator.cs
@@ -10,20 +10,46 @@ namespace SpaceInvadersRemake.Generator
     /// Die Klasse dient dazu sich Gegner (<c>GameItem</c>) generieren zu lassen
     /// </summary>
     /// <remarks>Abstrakte Fabrik</remarks>
-    public abstract class FormationGenerator
+    public static class FormationGenerator
     {
+        public static Array BlockFormation
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public static Array SkullFormation
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
 
        /// <summary>
         /// Generiert eine FormationEnum von Gegnern 
        /// </summary>
        /// <returns></returns>
-        protected abstract List<IGameItem> CreateFormation(int hitpoints, Vector2 velocity);
+        protected static List<IGameItem> CreateFormation(int hitpoints, Vector2 velocity, Array formation)
+        {
+            throw new System.NotImplementedException();
+}
  
     }
 
     public static class WaveGenerator
     {
-        public static Controller CreateWave(DifficultyLevel difficulty, FormationEnum formation, ControllerEnum AI)
+        public static event EventHandler WaveGenerated;
+    
+        public static List<IGameItem> CreateWave(ControllerEnum AI, Array formation, DifficultyLevel difficultyLevel)
         {
             throw new System.NotImplementedException();
             //SwitchCase über "Bestellung" 
