5465e83989442f5a8ee5634eac2019a08e8fd6c6
unrealegon <unrealegon@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-07 15:27:23 +0000

    [Model] Struct ControllerPara.. geändert zu ControllerEventArgs  weil es mit nem Struct doch nicht geht

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller/MenuController.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller/MenuController.cs
@@ -19,12 +19,18 @@ namespace SpaceInvadersRemake.Controller
     /// Für die Menüsteurung sind folgende Interaktionsmöglichkeiten vorgesehen:
     /// "Cursor" nach oben/unten
     /// Menüpunkt Bestätigen
-    /// Zurück ist durch die Esc Taste zu realisieren
-    /// Falls Menüpunkt Einstellungsmöglichkeiten bietet, Auswahl dieser durch links/rechts
+    /// Zurück 
+    /// Falls ein Menüpunkt Einstellungsmöglichkeiten bietet, werden diese mit rechts/links ausgewählt.
     /// </remarks>
     class MenuController : IController
     {
 
+        /// <summary>
+        /// Erlaubt die Ausführung der im Controller enthalten Spielmechanik.
+        /// </summary>
+        /// <param name="game">Referenz des Games aus dem XNA Framework.</param>
+        /// <param name="gameTime">Bietet die aktuelle Spielzeit an.</param>
+        /// <param name="state">Gibt den aktuellen State an von dem diese Funktion aufgerufen wurde.</param>
         public void Update(Game game, GameTime gameTime, State state)
         {
             throw new NotImplementedException();
