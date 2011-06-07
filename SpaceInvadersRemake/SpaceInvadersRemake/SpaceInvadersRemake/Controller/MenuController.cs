681c465960bb6a775bde7c005f3233427ce3658f
unrealegon <unrealegon@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-07 09:59:36 +0000

    [Controller] MenüController angefangen Achtung nicht fertig

---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/Controller/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using SpaceInvadersRemake.StateMachine;
+
+//TODO: Diskussion über Designentscheidungen
+
+// UNDER CONSTRUCTION -CK
+
+
+namespace SpaceInvadersRemake.Controller
+{
+    /// <summary>
+    /// Diese Klasse stellt die Menüsteuerung dar.
+    /// </summary>
+    /// <remarks>
+    /// Für die Menüsteurung sind folgende Interaktionsmöglichkeiten vorgesehen:
+    /// "Cursor" nach oben/unten
+    /// Menüpunkt Bestätigen
+    /// Zurück ist durch die Esc Taste zu realisieren
+    /// Falls Menüpunkt Einstellungsmöglichkeiten bietet, Auswahl dieser durch links/rechts
+    /// </remarks>
+    class MenuController : IController
+    {
+
+        public void Update(Game game, GameTime gameTime, State state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
