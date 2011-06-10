7a3740ed745669e9352d32516458bb914937dbc2
doominikthunder <doominikthunder@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-10 00:17:53 +0000

    [Model] Doku & Diverses bei WaveGenerator & Co. überarbeitet.

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/ModelSection/WaveGenerator.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/ModelSection/WaveGenerator.cs
@@ -8,22 +8,23 @@ namespace SpaceInvadersRemake.ModelSection
     /// <summary>
     /// Erzeugt neue Wellen anhand der drei Parameter AI, Formation und Schwierigkeitsgrad.
     /// </summary>
+    /// <remarks>Die Bezeichnung "Controller" bezieht sich im Folgenden auf den Controller-Bereich aus MVC.</remarks>
     public static class WaveGenerator
     {
         
         //UNDONE Klasse überprüfen wegen  Änderung nötig ! da struct als eventarg nicht funktioniert -CK
- 
+
 
         /// <summary>
-        /// Übergibt die benötigten Parameter an den Controllers über die "ControllerEventArgs".
+        /// Übergibt die benötigten Parameter an den Controller über die "ControllerEventArgs".
         /// </summary>
         /// <remarks>Parameter sind: Gewünschte AI als Enum, Liste der Gegner-Objekte und Schwierigkeitsgrad-Objekt.</remarks>
         public static event EventHandler<ControllerEventArgs> WaveGenerated;
 
         /// <summary>
-        /// Erzeugt eine neue Welle mit den Parametern AI, Formation und Schwierigkeitsgrad, und ruft danach das Event "WaveGenerated" auf, um dem Controllers die gewünschten Controllers-Eigenschaften mitzuteilen.
+        /// Erzeugt eine neue Welle mit den Parametern AI, Formation und Schwierigkeitsgrad, und ruft danach das Event "WaveGenerated" auf, um dem Controller die gewünschten Controller-Eigenschaften mitzuteilen.
         /// </summary>
-        /// <remarks>Dem Event "WaveGenerated" werden die gewünschte Controllers-AI, die erstellte Liste an Gegnern und das  Schwierigkeitsgrad-Objekt übergeben.</remarks>
+        /// <remarks>Dem Event "WaveGenerated" werden die gewünschte Controller-AI, die erstellte Liste an Gegnern und das Schwierigkeitsgrad-Objekt übergeben.</remarks>
         public static List<IGameItem> CreateWave(BehaviourEnum AI, Vector2[] formation, DifficultyLevel difficultyLevel)
         {
             throw new System.NotImplementedException();
