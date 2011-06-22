3dced8c1cfe2033bdd34070b07aa5e60722182b3
margerine <margerine@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-22 13:59:22 +0000

    [Model] - Interface zur Kollisionsberechnung hinzugefügt

---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/ModelSection/IBoundingVolume.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvadersRemake.ModelSection
+{
+    /// <summary>
+    /// Schnittstelle für Umgebungsvolumen. Bietet Funktionalität zur Kollisionsberechnung.
+    /// </summary>
+    public interface IBoundingVolume
+    {
+        /// <summary>
+        /// Überprüft ob sich das Umgebungsvolumen mit einem anderen überschneidet.
+        /// </summary>
+        /// <param name="other">Das andere Umgebungsvolumen</param>
+        /// <returns>Gibt an ob Überschneidung erfolgt</returns>
+        bool Intersects(IBoundingVolume other);
+    }
+}
