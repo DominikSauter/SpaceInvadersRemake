c69708ecfa2e6ee7387d539e4e704555ab7982f3
invaderanji <invaderanji@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-22 13:13:19 +0000

    [View] HighscoreUI (fertig)

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/View/HighscoreUI.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/View/HighscoreUI.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Implementiert von Anji
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,20 +17,21 @@ namespace SpaceInvadersRemake.View
     public class HighscoreUI : IView
     {
         private Texture2D highscoreBackgroundImage;
-        private Texture2D font;
+        private Texture2D frame;
+        private SpriteFont font;
+        private HighscoreManager highscoreManager;
+        private SpriteBatch spriteBatch;
+        private GraphicsDeviceManager graphics; //um Auflösung zu verändern
 
         /// <summary>
         /// Initialisiert die Highscoreoberfläche
         /// </summary>
-        public HighscoreUI(SpriteFont font, Texture2D background, HighscoreEntry[] highscoreEntries)
+        public HighscoreUI(SpriteFont font, Texture2D background, HighscoreManager highscoreManager, GraphicsDeviceManager graphics, Texture2D frame)
         {
-            throw new System.NotImplementedException();
-        }
-
-        public HighscoreEntry[] HighscoreEntries
-        {
-            get;
-            set;
+            this.font = ViewContent.UIContent.Font;
+            this.background = ViewContent.UIContent.MenuBackgroundImage;
+            this.frame = ViewContent.UIContent.SettingsBackground;
+    
         }
 
         /// <summary>
@@ -37,7 +40,45 @@ namespace SpaceInvadersRemake.View
         /// <param name="gameTime">Spielzeit</param>
         public void Draw(GameTime gameTime)
         {
-            throw new System.NotImplementedException();
+            //Zeichnen des Hintergrundbildes
+            spriteBatch.Begin();
+            spriteBatch.Draw(highscoreBackgroundImage, new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight),Color.White);
+            
+
+            //Zeichnen des Highscore Fensters
+            spriteBatch.Draw(frame, new Rectangle(100, 100, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferWidth), Color.White);
+
+            //Zeichnen der Highscore Einträge
+            Vector2 name_Position = new Vector2(200, 200);
+            Vector2 score_Position = new Vector2(400, 200);
+            String writeEnabled = "_";
+
+            //Incrementieren der Positionen und Zeichnen der Elemente an den Positionen
+            for (int i = 0; i < highscoreManager.HighscoreEntries.Length; i++) 
+            {
+                //Names
+                String name = highscoreManager.HighscoreEntries[i].Name;
+                if (highscoreManager.HighscoreEntries[i] == highscoreManager.NewEntry)
+                //Wenn ein neuer Eintrag möglich ist, wird ein Cursor ("_") an die letze Stelle der Liste gesetzt, 
+                //wo der neue Eintrag erfolgt werden kann. Ansonsten wird der Name gezeichnet.
+                {
+                    spriteBatch.DrawString(font, name + writeEnabled, name_Position, Color.Cyan);
+                }
+                else 
+                {
+                    spriteBatch.DrawString(font, name, name_Position, Color.White);
+                }
+
+                //Scores
+                int score = highscoreManager.HighscoreEntries[i].Score;
+                spriteBatch.DrawString(font, score.ToString(), score_Position, Color.White);
+
+                name_Position.Y += name_Position.Y + i * 100;
+                score_Position.Y += score_Position.Y + i * 100;
+            }
+
+          spriteBatch.End();
+            
         }
     }
 }
