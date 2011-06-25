6c9fd1d66814058f317e4d604f2f00bf5ace7940
doominikthunder <doominikthunder@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-25 01:01:50 +0000

    [Model] Klasse GameItemConstants für GameItem-Standardwerte erstellt.

---
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/ModelSection/GameItemConstants.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SpaceInvadersRemake
+{
+    /// <summary>
+    /// Diese Klasse enthält Standardwerte der GameItems.
+    /// </summary>
+    public static class GameItemConstants
+    {
+        /// <summary>
+        /// Die standardmäßige Schussfrequenz der Aliens in Schussanzahl pro Sekunde.
+        /// </summary>
+        public static float AlienShootingFrequency
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Die standardmäßige Geschwindigkeitserhöhung der Aliens in Erhöhungswert pro Sekunde.
+        /// </summary>
+        public static Vector2 AlienVelocityIncrease
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
