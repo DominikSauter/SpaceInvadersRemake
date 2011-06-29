b34c30f1e8296567b287afa6597a983941fe9eb2
margerine <margerine@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-06-29 11:02:55 +0000

    [Model] - Move bekommt jetzt gameTime übergeben

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/ModelSection/Player.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/ModelSection/Player.cs
@@ -100,8 +100,9 @@ namespace SpaceInvadersRemake.ModelSection
         /// Der übergebene Richtungsvektor wird vor der Multiplikation normalisiert.
         /// </remarks>
         /// <param name="direction">Bewegungsrichtung</param>
+        /// <param name="gameTime">Spielzeit</param>
         /// <returns>Boole'scher Wert, der angibt ob die Bewegung ohne Probleme durchgeführt werden konnte. <c>true</c>: erfolg; <c>false</c>: es gab Probleme</returns>
-        public override bool Move(Vector2 direction)
+        public override bool Move(Vector2 direction, GameTime gameTime)
         {
             direction.Normalize();
 
