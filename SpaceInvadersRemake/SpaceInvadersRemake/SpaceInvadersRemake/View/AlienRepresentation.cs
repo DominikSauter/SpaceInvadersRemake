432dd6b089d111be770f4cf4074fd9b615843233
moepdodo211 <moepdodo211@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-07-04 09:49:09 +0000

    AlienRepresentation kommentiert

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/View/AlienRepresentation.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/View/AlienRepresentation.cs
@@ -14,14 +14,18 @@ namespace SpaceInvadersRemake.View
     /// <summary>
     /// Stellt die auf dem Bildschirm sichtbaren Aliens dar.
     /// </summary>
-    /// <remarks>
-    /// Hält die PartikelEngines für Explosionen.
-    /// </remarks>
     public class AlienRepresentation : GameItemRepresentation
     {
+        //Handler um die Grafikeinstellungen zu tätigen.
         private GraphicsDeviceManager graphics;
+
+        //3D Modell mit dem das Alien angezeigt wird
         private Model model;
+
+        //Modell Textur, da jedem Alien eine zufällige Textur zugewiesen wird
         private Texture2D alienTexture;
+
+        //letzte Position des Aliens
         private Vector3 lastPosition;
 
         /// <summary>
@@ -38,11 +42,12 @@ namespace SpaceInvadersRemake.View
         /// </summary>
         /// <param name="alienGameItem">Alien Datenmodell</param>
         /// <param name="randomTexture">Wird zufällig generiert und kann unterschiedliche Texturen zuordnen.</param>
+        /// <param name="graphics">Verbindungselement zu Grafikkarte</param>
         public AlienRepresentation(Alien alienGameItem, int randomTexture, GraphicsDeviceManager graphics)
         {
             this.graphics = graphics;
             this.model = ViewContent.RepresentationContent.AlienModel;
-            GameItem = alienGameItem;
+            GameItem = alienGameItem;       //Hält das zugehörige Datenmodel des Aliens, für die "isAlive"-Prüfung
             this.lastPosition = PlaneProjector.Convert2DTo3D(GameItem.Position);
             this.World = Matrix.CreateWorld(this.lastPosition, Vector3.Backward, Vector3.Up);
 
@@ -58,20 +63,31 @@ namespace SpaceInvadersRemake.View
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// Zeichnet das 3D Modell auf den Bildschirm.
+        /// </summary>
+        /// <param name="spriteBatch">Wird zum Zeichnen von 2D Grafik benötigt</param>
         public override void Draw(SpriteBatch spriteBatch)
         {
+            //aktuelle Position des des 3D Modells
             Vector3 currentPosition = PlaneProjector.Convert2DTo3D(GameItem.Position);
             if (currentPosition.X > this.lastPosition.X || currentPosition.X < this.lastPosition.X
                 || currentPosition.Z > this.lastPosition.Z || currentPosition.Z < this.lastPosition.Z)
             {
-                //Falsche Positionierung im 3D-Raum gefixt - TB
+                /* Berechnet die neue Position des 3D Modells falls sich diese geändert haben sollte.
+                 * 
+                 * Falsche Positionierung im 3D-Raum gefixt - TB
+                 * */
                 this.World = Matrix.CreateWorld(currentPosition, Vector3.Backward, Vector3.Up);
-                //Damit verschiebst du die Aliens in jedem Frame um ihre eigene Position, das geht nicht gut - TB
-                //UNDONE: this.World *= Matrix.CreateTranslation(currentPosition); - TB
+                //Position aktualisieren
                 this.lastPosition = currentPosition;
             }
 
-
+            /*
+             * WICHTIG!
+             * Zurücksetzen des DepthStencils um eine fehlerfreie Kombination von 2D-Sprite und 3D-Model 
+             * zeichnen zu können.
+             * */
             this.graphics.GraphicsDevice.DepthStencilState = DepthStencilState.Default;
 
             foreach (ModelMesh mesh in model.Meshes)
