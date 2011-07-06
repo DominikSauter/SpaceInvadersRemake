9a98944f7b308539fd58c30213389c1ecde2b165
moepdodo211 <moepdodo211@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-07-06 13:39:27 +0000

    Kamerawinkel angepasst

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/View/ViewManager.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/View/ViewManager.cs
@@ -60,7 +60,7 @@ namespace SpaceInvadersRemake.View
                 /* Vorläufige Matrizen.
                 * Wir müssen noch besprechen wie groß unsre 3D Ebene wird (dementsprechend müssen wir Camera und Projection initialisieren)
                 * */
-                GameItemRepresentation.Camera = Matrix.CreateLookAt(new Vector3(0.0f, 800.0f, 500.0f), new Vector3(0.0f, 0.0f, 0.0f), Vector3.Up);
+                GameItemRepresentation.Camera = Matrix.CreateLookAt(new Vector3(0.0f, 800.0f, 500.0f), new Vector3(0.0f, 0.0f, 80.0f), Vector3.Up);
                 GameItemRepresentation.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(60),
                     graphics.PreferredBackBufferWidth / graphics.PreferredBackBufferHeight, 0.1f, 5000.0f);
                 
