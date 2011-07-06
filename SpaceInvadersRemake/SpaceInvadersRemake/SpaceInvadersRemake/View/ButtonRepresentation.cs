a65d456e8076655d0f8aca10ba21ddadbf5aa91c
invaderanji <invaderanji@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-07-06 20:14:33 +0000

    [View] ButtonRepresentation Fehler behoben

--- SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/View/ButtonRepresentation.cs
+++ SpaceInvadersRemake/SpaceInvadersRemake/SpaceInvadersRemake/View/ButtonRepresentation.cs
@@ -17,15 +17,11 @@ namespace SpaceInvadersRemake.View
     public class ButtonRepresentation
     {
         private MenuControl menuControl;
-        private Texture2D buttonTexture;
-        private Texture2D selectTexture; 
+        private Texture2D buttonTexture; //property, falls Menu Zentrieren
+        private Texture2D selectTexture; //evtl. auch property, falls Menu Zentrieren
         private SpriteFont font;
-        //Farbe für aktive Menü-Elemente
-        private Color activeColor; 
+        private Color activeColor; //Farbe für aktive Menü-Elemente
         private Color normalColor;
-
-        private Vector2 fontSize;
-        private Vector2 fontCenter;
 
 
         /// <summary>
@@ -37,47 +33,36 @@ namespace SpaceInvadersRemake.View
             this.menuControl = menuControl;
             this.font = ViewContent.UIContent.Font;
             this.buttonTexture = ViewContent.UIContent.MenuButton;
-            this.selectTexture = ViewContent.UIContent.SettingsButton;
+            this.selectTexture = ViewContent.UIContent.SettingsButton; //Der muss noch geändert werden
             this.normalColor = Color.White;
-            this.activeColor = new Color(0, 234, 255);
-            //Größe des Schriftzugs
-            this.fontSize = font.MeasureString(menuControl.Text);
-            //Mitte des Schriftzugs
-            this.fontCenter = fontSize / 2;
+            this.activeColor = new Color(0, 234, 255);     //0, 228, 255
         }
-        
+
         /// <summary>
         /// Zeichnet eine Schlatfläche
         /// </summary>
-        /// <param name="spriteBatch">spriteBatch</param>
+        /// <param name="spriteBatch"></param>
         /// <param name="position">Position für die Beschriftung der Schaltfläche</param>
         public void Draw(SpriteBatch spriteBatch, Vector2 position)
         {
-            //Größe des Schriftzugs
+
+            //TODO: schauen welche Vektoren in die schleifen kommen
+            //TODO: überlegen was tun wegen unterschiedlicher buttonLabel Länge, dass Select Buttons alle auf einer Ebene
+            //Idee: mit MeasureString längste Länge bestimmen, davon Abstand zu Select-Element
             Vector2 fontSize = font.MeasureString(menuControl.Text);
-            //Mitte des Schriftzugs
             Vector2 fontCenter = fontSize / 2;
 
-            //Position von Select-Feld und Select-Textur
-            Vector2 selectPosition = position + new Vector2(fontSize.X + 50, 0); 
-            Vector2 selectTextPosition = new Vector2(selectPosition.X + 20, selectPosition.Y); 
+            Vector2 selectPosition = position + new Vector2(fontSize.X + 50, 0); //Position des SelectButtons
+            Vector2 selectTextPosition = new Vector2(selectPosition.X + 20, selectPosition.Y); //Position der SelectAnzeige //zentrieren?
 
-            //Eingerückte Position des ausgewählten Buttons
-            Vector2 shiftPosition = new Vector2(position.X + 50, position.Y); 
+            Vector2 shiftPosition = new Vector2(position.X + 50, position.Y); //Eingerückte position
 
-            //Mitte des Buttons und des Select-Feldes
-            Vector2 buttonCenter = new Vector2(buttonTexture.Width / 2, buttonTexture.Height / 2); 
-            Vector2 selectFieldCenter = new Vector2(selectTexture.Width / 2, selectTexture.Height / 2); 
+            Vector2 buttonCenter = new Vector2(buttonTexture.Width / 2, buttonTexture.Height / 2); //Mittelpunkt des Buttons
+            Vector2 selectFieldCenter = new Vector2(selectTexture.Width / 2, selectTexture.Height / 2); //Mittelpunkt des Select-Feldes
 
-
-            //Setzt den Mittelpunkt des Textzugs in die Mitte des Buttons
-            Vector2 textCenter = position + buttonCenter;
-
-            //Setzt den Mittelpunkt des eingerückten Textzugs in die Mitte des eingerückten Buttons
-            Vector2 shiftTextCenter = shiftPosition + buttonCenter;
-
-            //Setzt den Mittelpunkt des Select-Textes in die Mitte des Select-Feldes
-            Vector2 selectTextCenter = selectPosition + selectFieldCenter; 
+            Vector2 textCenter = position + buttonCenter; //Setzt den Mittelpunkt des Textzugs in die Mitte des Buttons
+            Vector2 shiftTextCenter = shiftPosition + buttonCenter; //Setzt den Mittelpunkt des eingerückten Textzugs in die Mitte des eingerückten Buttons
+            Vector2 selectTextCenter = selectPosition + selectFieldCenter; //Setzt den Mittelpunkt des Select-Textes in die Mitte des Select-Feldes
 
 
             //Zeichnen eines Buttons
@@ -91,10 +76,10 @@ namespace SpaceInvadersRemake.View
                     //Buttontextur
                     spriteBatch.Draw(buttonTexture, shiftPosition, activeColor);
                     //Aktiver Button
-                    spriteBatch.DrawString(font, menuControl.Text, shiftTextCenter, activeColor, 0 , fontCenter, 1.0f, SpriteEffects.None, 0.5f);
+                    spriteBatch.DrawString(font, menuControl.Text, shiftTextCenter, activeColor, 0, fontCenter, 1.0f, SpriteEffects.None, 0.5f);
                 }
                 else
-                {                    
+                {
                     //Buttontextur
                     spriteBatch.Draw(buttonTexture, position, normalColor);
 
@@ -106,7 +91,8 @@ namespace SpaceInvadersRemake.View
             //Zeichnen eines Select-Buttons
             else if (menuControl is ListSelect) // Anpassung für beliebige ListSelect von Tobias
             {
-
+                Vector2 selectFontSize = font.MeasureString(((ListSelect)menuControl).SelectedItemText);
+                Vector2 selectFontCenter = selectFontSize / 2;
 
                 spriteBatch.Begin();
 
@@ -127,7 +113,7 @@ namespace SpaceInvadersRemake.View
 
             }
 
-          spriteBatch.End(); 
+            spriteBatch.End();
 
         }
     }
