07cf4d0239fed099957334234fdbc1838d58b1ff
margerine <margerine@12579820-0cf4-4278-bdb9-263b8477f6b5>
2011-08-11 08:57:01 +0000

    [Model] - Tests für Player und Enemy Move-Methode hinzugefügt

---
+++ SpaceInvadersRemake/SpaceInvaderRemakeUnitTest/EnemyTest.cs
@@ -0,0 +1,120 @@
+﻿using SpaceInvadersRemake.ModelSection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.Xna.Framework;
+
+namespace SpaceInvaderRemakeUnitTest
+{
+    
+    
+    /// <summary>
+    ///Dies ist eine Testklasse für "EnemyTest" und soll
+    ///alle EnemyTest Komponententests enthalten.
+    ///</summary>
+    [TestClass()]
+    public class EnemyTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Ruft den Testkontext auf, der Informationen
+        ///über und Funktionalität für den aktuellen Testlauf bietet, oder legt diesen fest.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Zusätzliche Testattribute
+        // 
+        //Sie können beim Verfassen Ihrer Tests die folgenden zusätzlichen Attribute verwenden:
+        //
+        //Mit ClassInitialize führen Sie Code aus, bevor Sie den ersten Test in der Klasse ausführen.
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Mit ClassCleanup führen Sie Code aus, nachdem alle Tests in einer Klasse ausgeführt wurden.
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Mit TestInitialize können Sie vor jedem einzelnen Test Code ausführen.
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Mit TestCleanup können Sie nach jedem einzelnen Test Code ausführen.
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        internal virtual Enemy CreateEnemy()
+        {
+            // TODO: Geeignete konkrete Klasse instanziieren
+            Enemy target = new Alien(Vector2.Zero, GameItemConstants.AlienVelocity, 10, 10, GameItemConstants.AlienWeapon, 10);
+            return target;
+        }
+
+        /// <summary>
+        ///Ein Test für "Move", bei dem ein Alien nicht über den Rand kommt
+        ///</summary>
+        [TestMethod()]
+        public void MoveTestNotOverBorder()
+        {
+            // GameItem-Liste initialisieren
+            GameItem.GameItemList = new System.Collections.Generic.LinkedList<IGameItem>();
+
+            Enemy target = CreateEnemy(); // TODO: Passenden Wert initialisieren
+            Vector2 direction = CoordinateConstants.Right; // TODO: Passenden Wert initialisieren
+            GameTime gameTime = new GameTime(new TimeSpan(0, 42, 42), new TimeSpan(166667)); // TODO: Passenden Wert initialisieren
+            bool expected = true; // TODO: Passenden Wert initialisieren
+            bool actual;
+            actual = target.Move(direction, gameTime);
+            Assert.AreEqual(expected, actual);
+            //Assert.Inconclusive("Überprüfen Sie die Richtigkeit dieser Testmethode.");
+
+            // GameItem-Liste leeren
+            GameItem.GameItemList.Clear();
+        }
+
+        /// <summary>
+        ///Ein Test für "Move", bei dem ein Alien über den Rand kommt
+        ///</summary>
+        [TestMethod()]
+        public void MoveTestOverBorder()
+        {
+            // GameItem-Liste initialisieren
+            GameItem.GameItemList = new System.Collections.Generic.LinkedList<IGameItem>();
+
+            Enemy target = CreateEnemy(); // TODO: Passenden Wert initialisieren
+            target.Position = new Vector2(CoordinateConstants.RightBorder, 0.0f);
+            Vector2 direction = CoordinateConstants.Right; // TODO: Passenden Wert initialisieren
+            GameTime gameTime = new GameTime(new TimeSpan(0, 42, 42), new TimeSpan(166667)); // TODO: Passenden Wert initialisieren
+            bool expected = false; // TODO: Passenden Wert initialisieren
+            bool actual;
+            actual = target.Move(direction, gameTime);
+            Assert.AreEqual(expected, actual);
+            //Assert.Inconclusive("Überprüfen Sie die Richtigkeit dieser Testmethode.");
+
+            // GameItem-Liste leeren
+            GameItem.GameItemList.Clear();
+        }
+    }
+}
---
+++ SpaceInvadersRemake/SpaceInvaderRemakeUnitTest/PlayerTest.cs
@@ -0,0 +1,96 @@
+﻿using SpaceInvadersRemake.ModelSection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.Xna.Framework;
+
+namespace SpaceInvaderRemakeUnitTest
+{
+    
+    
+    /// <summary>
+    ///Dies ist eine Testklasse für "PlayerTest" und soll
+    ///alle PlayerTest Komponententests enthalten.
+    ///</summary>
+    [TestClass()]
+    public class PlayerTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Ruft den Testkontext auf, der Informationen
+        ///über und Funktionalität für den aktuellen Testlauf bietet, oder legt diesen fest.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Zusätzliche Testattribute
+        // 
+        //Sie können beim Verfassen Ihrer Tests die folgenden zusätzlichen Attribute verwenden:
+        //
+        //Mit ClassInitialize führen Sie Code aus, bevor Sie den ersten Test in der Klasse ausführen.
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Mit ClassCleanup führen Sie Code aus, nachdem alle Tests in einer Klasse ausgeführt wurden.
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Mit TestInitialize können Sie vor jedem einzelnen Test Code ausführen.
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Mit TestCleanup können Sie nach jedem einzelnen Test Code ausführen.
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Ein Test für "Move"
+        ///</summary>
+        [TestMethod()]
+        public void MoveTest()
+        {
+            // GameItem-Liste initialisieren
+            GameItem.GameItemList = new System.Collections.Generic.LinkedList<IGameItem>();
+
+            Vector2 position = GameItemConstants.PlayerPosition; // TODO: Passenden Wert initialisieren
+            Vector2 velocity = GameItemConstants.PlayerVelocity; // TODO: Passenden Wert initialisieren
+            int hitpoints = 10; // TODO: Passenden Wert initialisieren
+            int damage = 10; // TODO: Passenden Wert initialisieren
+            Weapon weapon = GameItemConstants.PlayerWeapon; // TODO: Passenden Wert initialisieren
+            int lives = 3; // TODO: Passenden Wert initialisieren
+            Player target = new Player(position, velocity, hitpoints, damage, weapon, lives); // TODO: Passenden Wert initialisieren
+            Vector2 direction = CoordinateConstants.Left; // TODO: Passenden Wert initialisieren
+            GameTime gameTime = new GameTime(new TimeSpan(0, 42, 42), new TimeSpan(166667)); // TODO: Passenden Wert initialisieren
+            bool expected = true; // TODO: Passenden Wert initialisieren
+            bool actual;
+            actual = target.Move(direction, gameTime);
+            Assert.AreEqual(expected, actual);
+            //Assert.Inconclusive("Überprüfen Sie die Richtigkeit dieser Testmethode.");
+
+            // GameItem-Liste leeren
+            GameItem.GameItemList.Clear();
+        }
+    }
+}
